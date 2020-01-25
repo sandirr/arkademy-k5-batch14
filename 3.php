6ebb68d742297803c55558cf359c6c716f73d236
Andi Irsandi <Andi.irsandi765@gmail.com>
2020-01-25 13:14:05 +0800

    Update 3.php

--- 3.php
+++ 3.php
@@ -5,10 +5,6 @@ function count_handshake($hand)
     $hitung = 0;
     $loop = $hand - 1;
 
-    // Gunakan perulangan dan tidak menggunakan rumus matematika 
-    // n x (n-1)/2 atau ½.n.(n-1) ataupun yang lainnya! 
-    // Jika menggunakan rumus tersebut akan dinilai salah!
-
     for ($i = 1; $i < $hand; $i++) {
         $hitung += $loop--;
     }
